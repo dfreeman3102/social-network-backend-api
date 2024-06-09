a52983ca84a10f0c12ce50fa204915d2531ec2cd
dfreeman3102 <davidfreeman3102@gmail.com>
2024-06-09 14:48:41 -0400

    added user model, need to add thought and friendCount

---
+++ models/userModel.js
@@ -0,0 +1,49 @@
+const { Schema, model, default: mongoose } = require("mongoose");
+const friendCountSchema = require("./friendCount");
+const thoughtSchema = require("./thoughtModel");
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        "Please enter a valid email address",
+      ],
+    },
+    thoughts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "thought",
+      },
+    ],
+    friends: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+const User = model("user", userSchema);
+
+module.exports = User;
